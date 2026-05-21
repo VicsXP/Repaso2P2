b0e3757c126f7930975b355336a966803c0f0613
victo <victo@VICS>
2026-05-20 22:09:51 -0600

    Add project files.

---
+++ Repaso2P2/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿namespace Repaso2P2.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public string TituloAlbum { get; set; } = string.Empty;
+
+        public string ArtistaAlbum { get; set; } = string.Empty;
+
+        public DateTime FechaPublicacion { get; set; }
+
+        public List<Cancion> Canciones { get; set; } = new();
+    }
+}
---
+++ Repaso2P2/Models/Cancion.cs
@@ -0,0 +1,13 @@
+﻿namespace Repaso2P2.Models
+{
+    public class Cancion
+    {
+        public string NombreCancion { get; set; } = string.Empty;
+
+        public string Artista { get; set; } = string.Empty;
+
+        public double Duracion { get; set; }
+
+        public string Archivo { get; set; } = string.Empty;
+    }
+}
